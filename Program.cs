6a942dd041c2fc965feafb0b08f654b08ac12c2a
Thiago Souza Leme <thiago_leme@icloud.com>
2024-04-15 22:22:39 -0300

    Conexao banco Dapper

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿
+using BaltaDataAccess.Models;
+using Dapper;
+using Microsoft.Data.SqlClient;
+
+
+
+
+const string connectionString = "Server=localhost,1433clear;Database=balta;User ID=sa;Password=Virtual0522!=@#01";
+
+// Microsoft.Data.SqlClient
+
+
+// Dapper
+using (var connection = new SqlConnection(connectionString))
+{
+    var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
+    foreach (var category in categories)
+    {
+        Console.WriteLine($"{category.Id} - {category.Title}");
+    }
+}
+
+
